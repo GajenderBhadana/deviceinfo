d74bbbac13f9a4464efe333bdfb7a2844746d94d
gajender bhadana <monikachandilagurjar#gmail.com>
2022-03-30 13:06:57 +0530

    first commit

---
+++ src/app/classes/posts.ts
@@ -0,0 +1,17 @@
+export class Posts {
+  created_dt: string = '';
+  device_IMEI: string = '';
+  device_battery_level: string = '';
+  device_external_storage!: null;
+  device_id: string = '';
+  device_internal_storage: string = '';
+  device_manufacturer: string = '';
+  device_model: string = '';
+  device_os_version: string = '';
+  device_ram: string = '';
+  device_sim_no: string = '';
+  device_sno: string = '';
+  device_storage_used: string = '';
+  device_temprature: undefined;
+  minute_diff!: number;
+}
--- src/app/deviceinfo/deviceinfo.component.ts
+++ src/app/deviceinfo/deviceinfo.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Posts } from '../classes/posts';
 
 @Component({
   selector: 'app-deviceinfo',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./deviceinfo.component.css'],
 })
 export class DeviceinfoComponent implements OnInit {
+  objPosts: any;
+
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    // this.http.post('http://mdm.oasyscybernetics.in:9090/GetDeviceInfoUI');
+    var opost = new Posts();
+    opost.device_id = 'TEG9300MTOC07709';
+    opost.created_dt = '11-03-2022 10:43:15';
+    opost.device_IMEI = 'No detected sim';
+    opost.device_battery_level = '36';
+    opost.device_external_storage = null;
+    opost.device_internal_storage = '6 GB';
+    opost.device_manufacturer = 'OASYS';
+    opost.device_model = 'TEG9300-M';
+    opost.device_os_version = '9';
+    opost.device_ram = '1.83 GB';
+    opost.device_sim_no = 'No detected sim';
+    opost.device_sno = 'TEG9300MTOC07709';
+    opost.device_storage_used = '2 GB';
+    opost.device_temprature = undefined;
+    opost.minute_diff = 27464;
+
+    this.http
+      .post('http://mdm.oasyscybernetics.in:9090/GetDeviceInfoUI', opost)
+      .subscribe((data) => {
+        this.objPosts = data;
+        console.log(data);
+      });
   }
 }
